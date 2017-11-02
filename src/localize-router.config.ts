6b89da5c2576361685015a7638b09e128cde6398
ivankolev <ivankolev@gmail.com>
2017-11-02 00:16:15 -0400

    #86 replace deprecated OpaqueToken with InjectionToken

--- src/localize-router.config.ts
+++ src/localize-router.config.ts
@@ -1,16 +1,16 @@
-import { Inject, OpaqueToken, Provider } from '@angular/core';
+import {Inject, InjectionToken, Provider} from '@angular/core';
 
 /**
  * Guard to make sure we have single initialization of forRoot
- * @type {OpaqueToken<LocalizeRouterModule>}
+ * @type {InjectionToken<LocalizeRouterModule>}
  */
-export const LOCALIZE_ROUTER_FORROOT_GUARD = new OpaqueToken('LOCALIZE_ROUTER_FORROOT_GUARD');
+export const LOCALIZE_ROUTER_FORROOT_GUARD = new InjectionToken('LOCALIZE_ROUTER_FORROOT_GUARD');
 
 /**
  * Static provider for keeping track of routes
- * @type {OpaqueToken<Routes[]>}
+ * @type {InjectionToken<Routes[]>}
  */
-export const RAW_ROUTES = new OpaqueToken('RAW_ROUTES');
+export const RAW_ROUTES = new InjectionToken('RAW_ROUTES');
 
 /**
  * Type for Caching of default language
@@ -27,39 +27,37 @@ export namespace CacheMechanism {
 
 /**
  * Boolean to indicate whether to use cached language value
- * @type {OpaqueToken<boolean>}
+ * @type {InjectionToken<boolean>}
  */
-export const USE_CACHED_LANG = new OpaqueToken('USE_CACHED_LANG');
+export const USE_CACHED_LANG = new InjectionToken('USE_CACHED_LANG');
 /**
  * Cache mechanism type
- * @type {OpaqueToken<CacheMechanism>}
+ * @type {InjectionToken<CacheMechanism>}
  */
-export const CACHE_MECHANISM = new OpaqueToken('CACHE_MECHANISM');
+export const CACHE_MECHANISM = new InjectionToken('CACHE_MECHANISM');
 /**
  * Cache name
- * @type {OpaqueToken<string>}
+ * @type {InjectionToken<string>}
  */
-export const CACHE_NAME = new OpaqueToken('CACHE_NAME');
+export const CACHE_NAME = new InjectionToken('CACHE_NAME');
 
 /**
  * Type for default language function
  * Used to override basic behaviour
  */
-export interface DefaultLanguageFunction {
-  (languages: string[], cachedLang?: string, browserLang?: string): string;
-}
+export type DefaultLanguageFunction = (languages: string[], cachedLang?: string, browserLang?: string) => string;
 
 /**
  * Function for calculating default language
- * @type {OpaqueToken<DefaultLanguageFunction>}
+ * @type {InjectionToken<DefaultLanguageFunction>}
  */
-export const DEFAULT_LANG_FUNCTION = new OpaqueToken('DEFAULT_LANG_FUNCTION');
+export const DEFAULT_LANG_FUNCTION = new InjectionToken('DEFAULT_LANG_FUNCTION');
 
 /**
  * Boolean to indicate whether prefix should be set for single language scenarios
- * @type {OpaqueToken<boolean>}
+ * @type {InjectionToken<boolean>}
  */
-export const ALWAYS_SET_PREFIX = new OpaqueToken('ALWAYS_SET_PREFIX');
+export const ALWAYS_SET_PREFIX = new InjectionToken('ALWAYS_SET_PREFIX');
 
 /**
  * Config interface for LocalizeRouter
