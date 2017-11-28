0e300d5dfcf2543b3ae82929feb65e30970d38d1
Stephen Lautier <stephen.lautier@outlook.com>
2017-11-28 20:10:06 +0100

    fix(pipe): pipe was breaking components after angular 5 update

--- src/localize-router.pipe.ts
+++ src/localize-router.pipe.ts
@@ -1,21 +1,20 @@
-import { PipeTransform, Pipe, Injectable, ChangeDetectorRef } from '@angular/core';
+import { PipeTransform, Pipe, Injectable, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { LocalizeRouterService } from './localize-router.service';
 import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/observable/forkJoin';
 import { equals } from './util';
-
-const VIEW_DESTROYED_STATE = 128;
 
 @Injectable()
 @Pipe({
   name: 'localize',
   pure: false // required to update the value when the promise is resolved
 })
-export class LocalizeRouterPipe implements PipeTransform {
+export class LocalizeRouterPipe implements PipeTransform, OnDestroy {
+  private markForTransform = true;
   private value: string | any[] = '';
   private lastKey: string | any[];
   private lastLanguage: string;
-  private subscription: Subscription;
+  private changes$$: Subscription;
 
   /**
    * CTOR
@@ -23,8 +22,9 @@ export class LocalizeRouterPipe implements PipeTransform {
    * @param _ref
    */
   constructor(private localize: LocalizeRouterService, private _ref: ChangeDetectorRef) {
-    this.subscription = this.localize.routerEvents.subscribe(() => {
-      this.transform(this.lastKey);
+    this.changes$$ = this.localize.routerEvents.subscribe(() => {
+      this.markForTransform = true;
+      this._ref.markForCheck();
     });
   }
 
@@ -37,7 +37,7 @@ export class LocalizeRouterPipe implements PipeTransform {
     if (!query || query.length === 0 || !this.localize.parser.currentLang) {
       return query;
     }
-    if (equals(query, this.lastKey) && equals(this.lastLanguage, this.localize.parser.currentLang)) {
+    if (!this.markForTransform && equals(query, this.lastKey) && this.lastLanguage === this.localize.parser.currentLang) {
       return this.value;
     }
     this.lastKey = query;
@@ -46,11 +46,14 @@ export class LocalizeRouterPipe implements PipeTransform {
     /** translate key and update values */
     this.value = this.localize.translateRoute(query);
     this.lastKey = query;
-    // if view is already destroyed, ignore firing change detection
-    if ((<any> this._ref)._view.state & VIEW_DESTROYED_STATE) {
-      return this.value;
-    }
-    this._ref.detectChanges();
+    this.markForTransform = false;
     return this.value;
   }
+
+  ngOnDestroy() {
+    if (this.changes$$) {
+      this.changes$$.unsubscribe();
+    }
+  }
+
 }
