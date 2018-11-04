3b0fc21cc8d1b5ea6994c76e102962a1867140ed
Miroslav Jonaš <missing.manual@gmail.com>
2018-11-04 17:09:33 +0100

    tests(config): fix failing rx-compat dependency.

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -1,12 +1,4 @@
-/*
- * When testing with webpack and ES6, we have to do some extra
- * things to get testing to work right. Because we are gonna write tests
- * in ES6 too, we have to compile those as well. That's handled in
- * karma.conf.js with the karma-webpack plugin. This is the entry
- * file for webpack test. Just like webpack will create a bundle.js
- * file for our client, when we run test, it will compile and bundle them
- * all here! Crazy huh. So we need to do some setup
- */
+
 Error.stackTraceLimit = Infinity;
 
 require('core-js/es6');
@@ -17,42 +9,24 @@ require('ts-helpers');
 
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/proxy.js');
+require('zone.js/dist/sync-test');
+require('zone.js/dist/jasmine-patch');
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/proxy'); // since zone.js 0.6.15
-require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
 
 // RxJS
-require('rxjs/Rx');
+require('rxjs');
 
-var testing = require('@angular/core/testing');
-var browser = require('@angular/platform-browser-dynamic/testing');
+const testing = require('@angular/core/testing');
+const browser = require('@angular/platform-browser-dynamic/testing');
 
-testing.TestBed.initTestEnvironment(
+testing.getTestBed().initTestEnvironment(
   browser.BrowserDynamicTestingModule,
   browser.platformBrowserDynamicTesting()
 );
 
-/*
- * Ok, this is kinda crazy. We can use the context method on
- * require that webpack created in order to tell webpack
- * what files we actually want to require or import.
- * Below, context will be a function/object with file names as keys.
- * Using that regex we are saying look in ../src then find
- * any file that ends with spec.ts and get its path. By passing in true
- * we say do this recursively
- */
-var testContext = require.context('../tests', true, /\.spec\.ts/);
-
-/*
- * get all the files, for each file, call the context function
- * that will require the file and load it up here. Context will
- * loop and require those spec files here
- */
-function requireAll(requireContext) {
-  return requireContext.keys().map(requireContext);
-}
-
-// requires and returns all modules that match
-var modules = requireAll(testContext);
+// Then we find all the tests.
+const context = require.context('../tests', true, /\.spec\.ts$/);
+// And load the modules.
+const modules = context.keys().map(context);
